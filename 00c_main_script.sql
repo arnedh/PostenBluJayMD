7a17947e85388673962960b6750207ae1fbfb137
Halvorsen Arne <Arne.Halvorsen@posten.no>
2019-11-04 11:38:06 +0100

    adresses7 < 20191001

--- 00c_main_script.sql
+++ 00c_main_script.sql
@@ -1,26 +1,29 @@
 
 
 ----main script, ADAPTED
-.read 1_loadsources.sql
+.read 01_loadsources.sql
 --- extract usedsearchnames from BluJay
-.read 2_load_used.sql
-.read 3a_createrelations.sql
-.read 4_create_bases.sql
-.read 5c_create_scheduled_bases.sql 
+.read 02_load_used.sql
+.read 03a_createrelations.sql
+.read 04_create_bases.sql
+.read 05c_create_scheduled_bases.sql 
 
-.read 6_write_unwashed_addresses.sql
+.read 06_write_unwashed_addresses.sql
 --- run script over unwashed_adr.csv, creating washed_adr.csv
-.read 7a_process_washed_adresses.sql
-.read 8c_produce_scheduled_orders.sql 
+.read 07a_process_washed_adresses.sql
+.read 08c_produce_scheduled_orders.sql 
 
 ---- load relations to BluJay
 ---- load relationRegistrations to BluJay
 ---- load addresses to BluJay
 ---- run script over scheduledOrders.csv to load scheduledOrders to BluJay
 
-.read 9a_corrected_adr_and_rel.sql 
+.read 09a_corrected_adr_and_rel.sql 
 -----ADDED
 .read 10a_truncated_adr.sql 
 -----ADDED
 .read 11a_pibpk_logadr.sql 
+-----ADDED
+
+.read 12_ORD_translation.sql 
 -----ADDED
--- 12_ORD_translation.sql
+++ 12_ORD_translation.sql
@@ -1,7 +1,8 @@
+/*
 select distinct dept, short, a.enhetvakt,  ordrenummer, vaktnr, rutenr, rutenavn
 from AlystraShifts a, AlystraDepts adepts, depts where a.enhetvakt = adepts.enhetvakt 
 and adepts.Column1 = depts.Dept order by a.enhetvakt,  ordrenummer, vaktnr, rutenr, rutenavn;
-
+*/
 
 .once mappingORD.csv
 select distinct dept, short, a.enhetvakt,  ordrenummer, vaktnr
@@ -12,4 +13,4 @@ and adepts.Column1 = depts.Dept order by a.enhetvakt,  ordrenummer, vaktnr;
 .once mappingORD2.csv
 select distinct dept, short, a.enhetvakt,  ordrenummer, vaktnr, RUTENR,RUTENAVN, RUTE_FRAKL
 from AlystraShifts a, AlystraDepts adepts, depts where a.enhetvakt = adepts.enhetvakt 
-and adepts.Column1 = depts.Dept order by a.enhetvakt,  ordrenummer, vaktnr,  RUTENR,RUTENAVN, RUTE_FRAKL
+and adepts.Column1 = depts.Dept order by a.enhetvakt,  ordrenummer, vaktnr,  RUTENR,RUTENAVN, RUTE_FRAKL;
